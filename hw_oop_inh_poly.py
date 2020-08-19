9aee31799352adbc42a83ae06d854bdfa3a6785f
alexandrmotologa <al.motologa@gmail.com>
2020-08-20 00:48:28 +0300

    hw

--- hw_oop_inh_poly.py
+++ hw_oop_inh_poly.py
@@ -1,20 +1,24 @@
 # UI -> User Interface
 
+
 class Label():
+    n = 60
+
     def __init__(self, text):
         self.text = text
-    
-    def __str__(self):
-        return f"\n#{self.text:^30}#\n"
 
+    def __str__(self):
+        return f"\n#{self.text:^{self.n}}#\n"
 
 
 class EmphasedLabel(Label):
-    
+
     # OVERRIDING
-    
+
     def __str__(self):
-        return "\n" + "#"*32 + super().__str__().upper() + "#"*32 + "\n"
+        self.n = Label.n
+        nx = "#"*(Label.n + 2)
+        return "\n" + nx + super().__str__().upper() + nx + "\n"
 
 
 main_label = Label("Hello OOP!")
